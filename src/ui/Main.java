ae71cf1c4f2b76e2355c861e3e6c415659bbb182
Alexander <alexssjr2002@gmail.com>
2021-05-21 21:14:48 -0500

    ConsoleLineInterface association created

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,12 @@
 package ui;
 
 public class Main {
+	
+	private static ConsoleLineInterface cli;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		cli = new ConsoleLineInterface ();
+		cli.startProgram();
 	}
 
 }
